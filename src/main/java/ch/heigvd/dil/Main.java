28ff78f35ebb982f1621d395e935f84ce9ebbab8
Loic Rosset <loicrossetmp@gmail.com>
2022-03-10 12:33:52 +0100

    Ajout de la sous-commande clean

--- src/main/java/ch/heigvd/dil/Main.java
+++ src/main/java/ch/heigvd/dil/Main.java
@@ -1,7 +1,23 @@
 package ch.heigvd.dil;
 
-public class Main {
+import ch.heigvd.dil.subcommands.CleanCmd;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "Main", subcommands = {
+        CleanCmd.class
+})
+public class Main implements Callable<Integer> {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int rc = new CommandLine(new Main()).execute(args);
+        System.exit(rc);
+    }
+
+    @Override
+    public Integer call() {
+        CommandLine.usage(this, System.out);
+        return 0;
     }
 }
