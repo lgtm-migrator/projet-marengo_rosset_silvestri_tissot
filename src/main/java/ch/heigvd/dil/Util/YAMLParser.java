5695b3e13ddc43e96eea833ed1ab57cdc0f61581
Géraud Silvestri <silvestri.geraud@gmail.com>
2022-03-24 14:28:11 +0100

    refontes du parser

--- src/main/java/ch/heigvd/dil/Util/YAMLParser.java
+++ src/main/java/ch/heigvd/dil/Util/YAMLParser.java
@@ -3,17 +3,58 @@ package ch.heigvd.dil.Util;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+/**
+ * @author : Géraud Silvestri
+ * Mets à disposition un parser Yaml, retournant une map à partir d'un fichier ou d'une liste de String
+ */
 public class YAMLParser {
-    public Map<String, Object> read(String path) throws IOException {
-        InputStream inputStream = new FileInputStream(path);
+    /**
+     * Parse un fichier YAML et renvoi une map contenant les paires clé-valeur
+     *
+     * @param path chemin du fichier à parse
+     * @return toutes les données du fichier donné
+     * @throws IOException retourne une exception si le fichier n'est pas trouver ou valide
+     */
+    public Map<String, Object> parseFromFile(String path) throws Exception {
+        Map<String, Object> data = null;
+        try (InputStream inputStream = new FileInputStream(path)) {
+            Yaml yaml = new Yaml();
+            data = yaml.load(inputStream);
+        } catch (FileNotFoundException e) {
+            throw new FileNotFoundException("File not found");
+        } catch (Exception e) {
+            throw new IOException("File isn't a YAML file");
+        }
 
-        Yaml yaml = new Yaml();
-        Map<String, Object> data = yaml.load(inputStream);
-        inputStream.close();
+        return data;
+    }
+
+    /**
+     * Crée une map à partir d'une liste de String
+     *
+     * @param content liste de String correspondant chacune à une ligne
+     * @return la map contenant les données, vide si la liste n'est pas valide
+     */
+    public Map<String, Object> parseFromString(List<String> content) {
+        Map<String, Object> data = new HashMap<>();
+        List<String[]> temp = new ArrayList<>();
+
+        for (String line : content)
+            temp.add(line.split(":"));
+
+        for (String[] line : temp) {
+            if (line.length == 2)
+                data.put(line[0], line[1]);
+        }
+
         return data;
     }
 }
--- src/test/java/ch/heigvd/dil/Util/YAMLParserTest.java
+++ src/test/java/ch/heigvd/dil/Util/YAMLParserTest.java
@@ -1,17 +1,58 @@
 package ch.heigvd.dil.Util;
 
-import junit.framework.TestCase;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-public class YAMLParserTest extends TestCase {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class YAMLParserTest {
+    final String PATH = "src/test/resources/data.yml";
+    final String WRONG_FILE = "src/test/resources/data.txt";
+    final YAMLParser parser = new YAMLParser();
+
     @Test
-    public void testParser() {
-        YAMLParser parser = new YAMLParser();
-        try {
-            var data = parser.read("data.yml");
-            assertEquals(data.get("author"), "Bob");
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+    public void itShouldParseCorrectly() throws Exception {
+        var data = parser.parseFromFile(PATH);
+        assertEquals(data.get("author"), "Bob");
+        assertEquals(data.get("version"), "0.0.1");
+        assertEquals(data.get("date"), "today");
+    }
+
+    @Test
+    public void itShouldThrowAFileNotFound() {
+        assertThrows(FileNotFoundException.class, () -> parser.parseFromFile("resources/notAFile.txt"));
+    }
+
+    @Test
+    public void itShouldThrowOnNonValidFile() {
+        assertThrows(IOException.class, () -> parser.parseFromFile(WRONG_FILE));
+    }
+
+    @Test
+    public void itShouldParseCorrectlyAListOfLine() throws FileNotFoundException {
+        Scanner s = new Scanner(new File(WRONG_FILE));
+        ArrayList<String> list = new ArrayList<>();
+        while (s.hasNext()) {
+            list.add(s.next());
         }
+        s.close();
+        var data = parser.parseFromString(list);
+        assertEquals(data.get("author"), "Bob");
+        assertEquals(data.get("version"), "0.0.1");
+        assertEquals(data.get("date"), "today");
+    }
+
+    @Test
+    public void itShouldBeNullOnNonValidData() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("aaaaa");
+        var data = parser.parseFromString(list);
+        assertEquals(0, data.size());
     }
 }
