7ef2051aa66cb3fdaaaecb9b5e66ff09d76e1b52
Géraud Silvestri <silvestri.geraud@gmail.com>
2022-03-24 11:27:12 +0100

    fin des tests

--- src/test/java/ch/heigvd/dil/Utils/YAMLParserTest.java
+++ src/test/java/ch/heigvd/dil/Utils/YAMLParserTest.java
@@ -3,16 +3,13 @@ package ch.heigvd.dil.Utils;
 import junit.framework.TestCase;
 import org.junit.Test;
 
-import java.util.Map;
-
 public class YAMLParserTest extends TestCase {
     @Test
     public void testParser() {
-        Map<String, Object> data = null;
         YAMLParser parser = new YAMLParser();
         try {
-            data = parser.read("data.yml");
-            assertSame(data.get("author"), "Bob");
+            var data = parser.read("data.yml");
+            assertEquals(data.get("author"), "Bob");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
