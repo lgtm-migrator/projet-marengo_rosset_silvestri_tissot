45140b79a3a8d97710c2ae76ffa2200462a50938
retroock <olivier.tissot21@gmail.com>
2022-03-31 12:11:05 +0200

    La commande version est opérationnelle.

--- src/main/java/ch/heigvd/dil/Site.java
+++ src/main/java/ch/heigvd/dil/Site.java
@@ -6,7 +6,11 @@ import ch.heigvd.dil.subcommands.ServeCmd;
 import ch.heigvd.dil.subcommands.NewCmd;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.IVersionProvider;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.concurrent.Callable;
 
 @Command(name = "site", subcommands = {
@@ -14,8 +18,12 @@ import java.util.concurrent.Callable;
         BuildCmd.class,
         ServeCmd.class,
         NewCmd.class
-})
+},
+mixinStandardHelpOptions = true,
+versionProvider = Site.VersionProvider.class)
 public class Site implements Callable<Integer> {
+    private static final String CONFIG_FILE = "project.properties";
+
     public static void main(String[] args) {
         int rc = new CommandLine(new Site()).execute(args);
         System.exit(rc);
@@ -25,5 +33,18 @@ public class Site implements Callable<Integer> {
     public Integer call() {
         CommandLine.usage(this, System.out);
         return 0;
+    }
+
+    static class VersionProvider implements IVersionProvider {
+        public String[] getVersion() {
+            Properties prop = new Properties();
+            try {
+                InputStream t = VersionProvider.class.getClassLoader().getResourceAsStream(CONFIG_FILE);
+                prop.load(t);
+                return new String[] { "Version du logiciel: " + prop.getProperty("version") };
+            } catch (IOException e) {
+                return new String[] { "Unable to read from " + CONFIG_FILE + ": " + e };
+            }
+        }
     }
 }
