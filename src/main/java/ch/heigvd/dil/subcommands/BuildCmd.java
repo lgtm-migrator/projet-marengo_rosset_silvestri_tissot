a40092cf42fbbc1b7a258561602b007af5c85785
BernardLhermite <stephane.marengo@hotmail.com>
2022-04-22 09:30:31 +0200

    Création d'un converter pour valider les Path
    Signed-off-by: BernardLhermite <stephane.marengo@hotmail.com>

--- src/main/java/ch/heigvd/dil/subcommands/BuildCmd.java
+++ src/main/java/ch/heigvd/dil/subcommands/BuildCmd.java
@@ -1,11 +1,22 @@
 package ch.heigvd.dil.subcommands;
 
+import static picocli.CommandLine.Command;
+import static picocli.CommandLine.Parameters;
 
+import ch.heigvd.dil.converter.PathDirectoryConverter;
+import java.nio.file.Path;
 import java.util.concurrent.Callable;
-import picocli.CommandLine.Command;
 
-@Command(name = "build", description = "Subcommand build !")
+/**
+ * @author Stéphane Marengo
+ */
+@Command(name = "build", description = "Builds the site")
 public class BuildCmd implements Callable<Integer> {
+    private static final String BUILD_DIR = "build";
+
+    @Parameters(description = "Path to the sources directory", converter = PathDirectoryConverter.class)
+    private Path path;
+
     @Override
     public Integer call() {
         System.out.println("Build command !");
