00c21589786bf4156a01ee1729805a9658357182
BernardLhermite <stephane.marengo@hotmail.com>
2022-04-24 13:26:49 +0200

    Refactor de la classe de base des tests
    Signed-off-by: BernardLhermite <stephane.marengo@hotmail.com>

--- src/test/java/ch/heigvd/dil/subcommands/CleanCmdTest.java
+++ src/test/java/ch/heigvd/dil/subcommands/CleanCmdTest.java
@@ -1,58 +1,45 @@
 package ch.heigvd.dil.subcommands;
 
-import java.io.File;
+import static org.junit.jupiter.api.Assertions.*;
+import static picocli.CommandLine.ExitCode;
+
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Comparator;
-import java.util.stream.Stream;
-import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Tissot Olivier
  * @author Stéphane Marengo
  */
-public class CleanCmdTest extends BaseCmdTest {
-    private static final String NAME_SITE = "SiteClearCmdTest";
-    private static final Path PATH = Paths.get(NAME_SITE);
-    private static final String BUILD = "build";
-    private static final String FILE = "file.txt";
-
-    private static final String DIRECTORY = "dossierTest";
-
+class CleanCmdTest extends BaseCmdTest {
+    @Override
     protected String getCommandName() {
         return "clean";
     }
 
-    @AfterAll
-    static void clearConfig() throws IOException {
-        if (!Files.exists(PATH)) return;
-
-        try (Stream<Path> walk = Files.walk(PATH)) {
-            walk.sorted(Comparator.reverseOrder()).map(Path::toFile).forEach(File::delete);
-        }
+    @BeforeEach
+    protected void setUp() throws IOException {
+        buildBasicSite();
     }
 
-    @BeforeEach
-    public void createBuildFiles() throws IOException {
-        clearConfig();
-
-        if (!Files.exists(PATH)) {
-            Files.createDirectory(PATH);
-            Files.createDirectory(Paths.get(NAME_SITE, BUILD));
-            Files.createFile(Paths.get(NAME_SITE, BUILD, FILE));
-            Files.createDirectory(Paths.get(NAME_SITE, BUILD, DIRECTORY));
-            Files.createFile(Paths.get(NAME_SITE, BUILD, DIRECTORY, FILE));
-        }
+    @AfterEach
+    protected void clean() throws IOException {
+        deleteBasicSite();
     }
 
     @Test
-    public void itShouldDeleteTheBuildDirAndItsContent() {
-        execute(NAME_SITE);
-        assertFalse(Files.exists(Paths.get(NAME_SITE, BUILD)));
+    void itShouldDeleteTheBuildDirAndItsContent() {
+        assertTrue(Files.isDirectory(BUILD_PATH));
+        assertEquals(ExitCode.OK, execute(TEST_DIRECTORY.toString()));
+        assertFalse(Files.isDirectory(BUILD_PATH));
+    }
+
+    @Test
+    void itShouldNotThrowIfBuildDirDoesNotExist() throws IOException {
+        cleanBuild();
+        assertFalse(Files.isDirectory(BUILD_PATH));
+        assertEquals(ExitCode.OK, execute(TEST_DIRECTORY.toString()));
     }
 }
--- src/test/java/ch/heigvd/dil/subcommands/ServeCmdTest.java
+++ src/test/java/ch/heigvd/dil/subcommands/ServeCmdTest.java
@@ -1,13 +1,13 @@
 package ch.heigvd.dil.subcommands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static picocli.CommandLine.ExitCode;
 
-import ch.heigvd.dil.util.FilesHelper;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -16,32 +16,20 @@ import org.junit.jupiter.api.Test;
 class ServeCmdTest extends BaseCmdTest {
     private static final String INVALID_PATH = "/*invalidPath";
     private static final String NOT_A_DIRECTORY = "notADirectory/";
-    private static final Path BUILD_PATH = TEST_DIRECTORY.resolve(BuildCmd.BUILD_DIR);
-
-    /**
-     * Construit le site.
-     */
-    private void buildSite() {
-        // TODO ne devrait pas dépendre d'un autre test
-        executeCmd("build", TEST_DIRECTORY.toString());
-        assertEquals(ExitCode.OK, getReturnCode());
-    }
-
-    /**
-     * Supprime le dossier de build s'il existe.
-     * @throws IOException si un problème survient lors de la suppression
-     */
-    @AfterEach
-    private void cleanBuild() throws IOException {
-        if (!Files.isDirectory(BUILD_PATH)) return;
-
-        FilesHelper.cleanDirectory(BUILD_PATH);
-        Files.delete(BUILD_PATH);
-    }
 
     @Override
     protected String getCommandName() {
         return "serve";
+    }
+
+    @BeforeEach
+    protected void setUp() throws IOException {
+        buildBasicSite();
+    }
+
+    @AfterEach
+    protected void clean() throws IOException {
+        deleteBasicSite();
     }
 
     @Test
@@ -60,7 +48,9 @@ class ServeCmdTest extends BaseCmdTest {
     }
 
     @Test
-    void itShouldThrowOnMissingBuild() {
+    void itShouldThrowOnMissingBuild() throws IOException {
+        cleanBuild();
+        assertFalse(Files.isDirectory(BUILD_PATH));
         assertEquals(ExitCode.USAGE, execute(TEST_DIRECTORY.toString()));
     }
 
